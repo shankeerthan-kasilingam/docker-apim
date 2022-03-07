aabf479eeb889466d812aa3d6d8314684111d8d1
Ubuntu <shankeerthan1995@gmail.com>
2022-03-07 07:59:57 +0000

    tm1 changed files

--- docker-compose/apim-is-as-km-with-analytics/dockerfiles/tm/docker-entrypoint.sh
+++ docker-compose/apim-is-as-km-with-analytics/dockerfiles/tm/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Optimizing WSO2 Carbon Server" 
-sh ${WSO2_SERVER_HOME}/bin/profileSetup.sh 
-# sh ${WSO2_SERVER_HOME}/bin/profileSetup.sh -Dprofile=traffic-manager
-
-# sh ${WSO2_SERVER_HOME}/bin/api-manager.sh -Dprofile=traffic-manager "$@"
+#sh ${WSO2_SERVER_HOME}/bin/profileSetup.sh 
+sh ${WSO2_SERVER_HOME}/bin/profileSetup.sh -Dprofile=traffic-manager
+sh ${WSO2_SERVER_HOME}/bin/api-manager.sh -Dprofile=traffic-manager "$@"
